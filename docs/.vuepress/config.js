3cdbee35cc9418ddf3a3f82628a48422ac452935
rck <296046734@qq.com>
2022-02-28 23:23:57 +0800

    first

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    title: "Css",
+    base: '/css/',
+    themeConfig: {
+      nav: [{
+        text: 'Home', link: '/'
+      }, {
+        text: 'Github', link: 'https://github.com/l1872427108/uui'
+      }, {
+        text: 'Npm', link: 'https://www.npmjs.com/package/uu-uui'
+      }],
+      sidebar: [
+        '/',
+      ]
+    }
+  }
+  
